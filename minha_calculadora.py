8cee15997a1843e7fdbd5ae770409365f34b3c3d
Michael Queen <michael.queen@impacta.edu.br>
2026-03-27 08:59:58 -0300

    Segunda Versão

--- minha_calculadora.py
+++ minha_calculadora.py
@@ -1,9 +1,13 @@
 def soma(a, b):
     return a + b
 
+def subtracao(a,b):
+    return a - b
+
 def main():
     x = int(input("Digite um número: "))
     y = int(input("Digite outro número: "))
     print(f"A soma de {x} e {y} é {soma(x, y)}")
+    print(f"A subtração de {x} e {y} é {subtracao(x, y)}")
 
 main()
