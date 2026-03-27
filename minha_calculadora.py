4d3fe8ac65bfa82c1d28ec0b345281718d693554
Michael Queen <michael.queen@impacta.edu.br>
2026-03-27 08:54:04 -0300

    Primeira Versão da calculadora

---
+++ minha_calculadora.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    return a + b
+
+def main():
+    x = int(input("Digite um número: "))
+    y = int(input("Digite outro número: "))
+    print(f"A soma de {x} e {y} é {soma(x, y)}")
+
+main()
